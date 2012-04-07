83167e6af9565f92c4b2e22922e64bedc03fd3b4
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-06 17:59:15 -0700

    added video streaming possibly

--- android/src/com/sharad/android/MainActivity.java
+++ android/src/com/sharad/android/MainActivity.java
@@ -9,28 +9,112 @@ import java.net.*;
 
 public class MainActivity extends Activity
 {
-  EditText textOut;
-  TextView textIn;
-
+  // User Interface Elements
+  VideoView mView;
+  TextView connectionStatus;
+  SurfaceHolder mHolder;
+  // Video variable
+  MediaRecorder recorder; 
+  // Networking variables
+  public static String SERVERIP="";
+  public static final int SERVERPORT = 1234;
+  private Handler handler = new Handler();
+  private ServerSocket serverSocket;  
   /** Called when the activity is first created. */
   @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
       if (android.os.Build.VERSION.SDK_INT > 9) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
           .permitAll().build();
         StrictMode.setThreadPolicy(policy);
       }
-      socket = new Socket("192.158.1.3", 1234);
-      ParcelFileDescriptor  pfd =ParcelFileDescriptor.fromSocket(socket);
-      recorder = new MediaRecorder();
-      recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
-      recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-      recorder.setOutputFile(pfd.getFileDescriptor());
-      recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-      mPreview = new Preview(VideoRecorder.this,recorder);
-
-      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-      setContentView(mPreview);
+      // Define UI elements
+      mView = (VideoView) findViewById(R.id.video_preview);
+      connectionStatus = (TextView) findViewById(R.id.connection_status_textview);
+      mHolder = mView.getHolder();
+      mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+      SERVERIP = "192.168.1.3";
+      // Run new thread to handle socket communications
+      Thread sendVideo = new Thread(new SendVideoThread());
+      sendVideo.start();
     }
+  public class SendVideoThread implements Runnable{
+    public void run(){
+      // From Server.java
+      try {
+        if(SERVERIP!=null){
+          handler.post(new Runnable() {
+            @Override
+            public void run() {
+              connectionStatus.setText("Listening on IP: " + SERVERIP);
+            }
+          });
+          serverSocket = new ServerSocket(SERVERPORT);
+          while(true) {
+            //listen for incoming clients
+            Socket client = serverSocket.accept();
+            handler.post(new Runnable(){
+              @Override
+              public void run(){
+                connectionStatus.setText("Connected.");
+              }
+            });
+            try{
+              // Begin video communication
+              final ParcelFileDescriptor pfd = ParcelFileDescriptor.fromSocket(client);
+              handler.post(new Runnable(){
+                @Override
+                public void run(){
+                  recorder = new MediaRecorder();
+                  recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                  recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);                 
+                  recorder.setOutputFile(pfd.getFileDescriptor());
+                  recorder.setVideoFrameRate(20);
+                  recorder.setVideoSize(176,144);
+                  recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H263);
+                  recorder.setPreviewDisplay(mHolder.getSurface());
+                  try {
+                    recorder.prepare();
+                  } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                  } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                  }
+                  recorder.start();
+                }
+              });
+            } catch (Exception e) {
+              handler.post(new Runnable(){
+                @Override
+                public void run(){
+                  connectionStatus.setText("Oops.Connection interrupted. Please reconnect your phones.");
+                }
+              });
+              e.printStackTrace();
+            }
+          }
+        } else {
+          handler.post(new Runnable() {
+            @Override
+            public void run(){
+              connectionStatus.setText("Couldn't detect internet connection.");
+            }
+          });
+        }
+      } catch (Exception e){
+        handler.post(new Runnable() {
+          @Override
+          public void run() {
+            connectionStatus.setText("Error");
+          }
+        });
+        e.printStackTrace();
+      }
+      // End from server.java
+    }
+  }
 }
