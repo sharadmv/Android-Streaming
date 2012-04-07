25c72eb0ae3edeaeeeea61364f1854558e2a9457
Sharad Vikram <sharad.vikram@gmail.com>
2012-04-06 19:54:15 -0500

    able to stream socket data

--- android/src/com/sharad/android/MainActivity.java
+++ android/src/com/sharad/android/MainActivity.java
@@ -2,6 +2,8 @@ package com.sharad.android;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 import java.io.*;
 import java.net.*;
 
@@ -15,6 +17,11 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.main);
+	  if (android.os.Build.VERSION.SDK_INT > 9) {
+		  StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+			  .permitAll().build();
+		  StrictMode.setThreadPolicy(policy);
+	  }
 
       textOut = (EditText)findViewById(R.id.textout);
       Button buttonSend = (Button)findViewById(R.id.send);
@@ -29,14 +36,16 @@ public class MainActivity extends Activity
         public void onClick(View arg0) {
           // TODO Auto-generated method stub
           Socket socket = null;
-          DataOutputStream dataOutputStream = null;
+          OutputStream dataOutputStream = null;
           DataInputStream dataInputStream = null;
 
           try {
-            socket = new Socket("192.168.1.101", 8888);
+            socket = new Socket("192.168.1.3", 1234);
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataInputStream = new DataInputStream(socket.getInputStream());
-            dataOutputStream.writeUTF(textOut.getText().toString());
+			PrintWriter out = new PrintWriter(dataOutputStream);
+            out.print(textOut.getText().toString());
+			out.close();
             textIn.setText(dataInputStream.readUTF());
           } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
